c07a25f45722fe7b73caf09f172c1b00666911bc
Leo Li (FTVB-IT) 李俊波 <leo.li@gftgroup.com>
2021-08-23 17:01:47 +0700

    topnav

---
+++ src/components/TopNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="topNav">
+    <div class="topLeft">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-liebiao"></use>
+      </svg>
+    </div>
+    <div class="topCenter">
+      <span class="navBtn">我的</span>
+      <span class="navBtn">发现</span>
+      <span class="navBtn">云村 </span>
+      <span class="navBtn">视频</span>
+    </div>
+    <div class="topRight">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-search"></use>
+      </svg>
+    </div>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <TopNav>></TopNav>
+</template>
+
+<script>
+// @ is an alias to /src
+    
+import TopNav from '../components/TopNav.vue'
+export default {
+  name: "Home",
+  components: {TopNav},
+};
+</script>
